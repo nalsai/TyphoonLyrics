ef78b0ebf70d96c40c05db5309861137451ad8d7
Nils Fürniß <contact@nalsai.de>
2024-05-29 00:30:11 +0900

    Implement basic vue.js ui

--- README.md
+++ README.md
@@ -3,8 +3,8 @@
 > [!CAUTION]
 > This project is still in development and not yet ready for use.
 
-SyncedLyrics-UI is a frontend for the [syncedlyrics](https://github.com/moehmeni/syncedlyrics) Python library and CLI.
-It provides a web server for easy usage, both locally and directly on your media server.
+SyncedLyrics-UI is a frontend for the [syncedlyrics](https://github.com/moehmeni/syncedlyrics) Python library and CLI program.
+It uses a simple Flask web server for easy usage locally or directly on your media server.
 The goal of this project is to allow users to easily download the appropriate .lrc file for every music file they have.
 
 Thanks to syncedlyrics supporting a wide array of [providers](https://github.com/moehmeni/syncedlyrics?tab=readme-ov-file#providers),
@@ -12,12 +12,12 @@ SyncedLyrics-UI can find lyrics for almost any song that has synced lyrics avail
 
 ## Installation
 
-To install and run Syncedlyrics-UI, follow these steps:
+To install and run SyncedLyrics-UI, follow these steps:
 
 1. Clone the repository:
 
     ```shell
-    git clone https://github.com/your-username/syncedlyrics-ui.git
+    git clone https://github.com/nalsai/syncedlyrics-ui.git
     ```
 
 2. Install the required dependencies:
@@ -35,7 +35,7 @@ To install and run Syncedlyrics-UI, follow these steps:
 ## Usage
 
 Once the web server is running, you can access SyncedLyrics-UI by opening your browser and navigating to `http://localhost:5000`.
-From there, you can search for music files and download the corresponding .lrc files.
+From there, you can input the path to search for music files and download the lyrics.
 
 ## License
 
--- app.py
+++ app.py
@@ -1,56 +1,127 @@
 import os
-from flask import Flask, request, render_template, redirect, url_for
+import threading
+from flask import Flask, request, jsonify, render_template
 import syncedlyrics
 from tinytag import TinyTag
 
 app = Flask(__name__)
 
-# Supported music file extensions
-MUSIC_EXTENSIONS = ('.flac', '.opus', '.ogg', '.mp3', '.wav', '.m4a', '.aac', '.alac', '.wma', '.aiff', '.aif', '.aifc')
+MUSIC_EXTENSIONS = (".mp3", ".flac", ".wav", ".m4a")
+
+file_states = {}
+working_directory = ""
+downloading = False
+
+# Configuration
+delete_lyric_txt = True
+refresh_existing_lyrics = False
+
+
+def remove_prefix(text, prefix):
+    """Remove the given prefix from the text."""
+    if text.startswith(prefix):
+        return text[len(prefix) :]
+    return text
+
 
 def find_music_files(directory):
     """Recursively find all music files in the given directory and its subdirectories."""
     music_files = []
+    global working_directory
+    working_directory = directory
+    print(working_directory)
     for root, _, files in os.walk(directory):
         for file in files:
             if file.lower().endswith(MUSIC_EXTENSIONS):
-                music_files.append(os.path.join(root, file))
+                full_file_path = os.path.join(root, file)
+                file_path = remove_prefix(full_file_path, working_directory)
+                music_files.append(file_path)
+
+                path_without_extension = os.path.splitext(full_file_path)[0]
+                lyric_file = path_without_extension + ".lrc"
+                if os.path.exists(lyric_file):
+                    file_states[file_path] = "Lyrics already present"
+                else:
+                    file_states[file_path] = "Pending"
     return music_files
+
 
 def download_lyrics_for_music_files(music_files):
     """Download lyrics for the given list of music files."""
     for music_file in music_files:
+        global downloading
+        downloading = True
+
         try:
-            tag = TinyTag.get(music_file)
+            if (
+                refresh_existing_lyrics
+                and file_states[music_file] == "Lyrics already present"
+            ):
+                file_states[music_file] = "Pending"
+            if file_states[music_file] != "Pending":
+                continue
 
-            lyrics = syncedlyrics.search("["+tag.title+"] ["+tag.artist+"]") 
-            # TODO: try different search queries
-            # TODO: add karoke lyrics and translation support
+            path = working_directory + music_file
+            tag = TinyTag.get(path)
+
+            lyrics = syncedlyrics.search("[" + tag.title + "] [" + tag.artist + "]")
+            """
+            TODO:
+            - use save_path to save the lyrics?
+            - make sure the lyrics are for the correct song
+            - compare different search results and select the best one, allow user to select a different one
+            - add karoke lyrics and translation support
+            - add option to allow for plain text lyrics
+            """
             if lyrics:
-                lyric_file = music_file + '.lrc'
-                with open(lyric_file, 'w', encoding='utf-8') as f:
+                path_without_extension = os.path.splitext(path)[0]
+                lyric_file = path_without_extension + ".lrc"
+                with open(lyric_file, "w", encoding="utf-8") as f:
                     f.write(lyrics)
-                print(f"Lyrics downloaded for: {music_file}")
+                file_states[music_file] = "Downloaded"
+
+                lyric_txt_file = path_without_extension + ".txt"
+                if os.path.exists(lyric_txt_file) and delete_lyric_txt:
+                    os.remove(lyric_txt_file)
             else:
-                print(f"No lyrics found for: {music_file}")
+                file_states[music_file] = "No lyrics found"
         except Exception as e:
-            print(f"Error downloading lyrics for {music_file}: {e}")
+            file_states[music_file] = f"Error: {str(e)}"
 
-@app.route('/', methods=['GET', 'POST'])
+        downloading = False
+
+
+@app.route("/")
 def index():
-    if request.method == 'POST':
-        directory = request.form['directory']
-        if os.path.isdir(directory):
-            music_files = find_music_files(directory)
-            download_lyrics_for_music_files(music_files)
-            return redirect(url_for('success'))
-        else:
-            return render_template('index.html', error="Invalid directory. Please try again.")
-    return render_template('index.html')
+    return render_template("index.html", downloading= "true" if downloading else "false")
 
-@app.route('/success')
-def success():
-    return "Lyrics downloaded successfully!"
 
-if __name__ == '__main__':
+@app.route("/files", methods=["POST"])
+def get_files():
+    directory = request.json.get("directory")
+    if os.path.isdir(directory):
+        music_files = find_music_files(directory)
+        return jsonify({"files": music_files, "states": file_states})
+    else:
+        return jsonify({"error": "Invalid directory."}), 400
+
+
+@app.route("/download_lyrics", methods=["POST"])
+def start_download():
+    music_files = request.json.get("files")
+    if music_files:
+        threading.Thread(
+            target=download_lyrics_for_music_files, args=(music_files,)
+        ).start()
+        return jsonify({"status": "Download started"})
+    else:
+        return jsonify({"error": "No files provided."}), 400
+
+
+@app.route("/states", methods=["GET"])
+def get_states():
+    return jsonify(file_states)
+
+
+if __name__ == "__main__":
     app.run(debug=True)
